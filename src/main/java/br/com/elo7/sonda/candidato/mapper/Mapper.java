45092ab0cda08487664a0e391e6e8af1720d62d6
Rafael <rafael_colvara@hotmail.com>
2022-08-14 17:37:05 -0300

    Add mapper to planet Pojo

--- src/main/java/br/com/elo7/sonda/candidato/mapper/Mapper.java
+++ src/main/java/br/com/elo7/sonda/candidato/mapper/Mapper.java
@@ -1,5 +1,6 @@
 package br.com.elo7.sonda.candidato.mapper;
 
+import br.com.elo7.sonda.candidato.dto.InputDTO;
 import br.com.elo7.sonda.candidato.dto.ProbeDTO;
 import br.com.elo7.sonda.candidato.model.Planet;
 import br.com.elo7.sonda.candidato.model.Probe;
@@ -17,4 +18,12 @@ public class Mapper {
         probe.setDirection(probeDto.getDirection());
         return probe;
     }
+
+    public Planet InputDTOToPlanet(InputDTO input) {
+        Planet planet = new Planet();
+        planet.setHeight(input.getHeight());
+        planet.setWidth(input.getWidth());
+        return planet;
+    }
+
 }
