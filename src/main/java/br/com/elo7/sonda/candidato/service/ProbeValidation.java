7fc918cb4014652e77153b12f304a102549c7022
Rafael Colvara <rafael.colvara@valemobi.com.br>
2022-08-27 16:40:29 -0300

    Isolating Validations

--- src/main/java/br/com/elo7/sonda/candidato/service/ProbeValidation.java
+++ src/main/java/br/com/elo7/sonda/candidato/service/ProbeValidation.java
@@ -1,2 +1,26 @@
-package br.com.elo7.sonda.candidato.service;public class ProbeValidation {
+package br.com.elo7.sonda.candidato.service;
+
+import br.com.elo7.sonda.candidato.model.Probe;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+@Component
+public class ProbeValidation {
+
+    public Boolean validMoveProbe(Probe probe, int newX, int newY) {
+        return !(((probe.getPlanet().getHeight() <= newX) || (probe.getPlanet().getWidth() <= newY)) || (newX < 0 || newY < 0));
+    }
+
+    public Boolean checkColisionProbes(Probe probe){
+
+        List<Probe> probeList = probe.getPlanet().getProbeList()
+                .stream().filter(
+                        probe1 -> probe1.equals(probe)
+                ).collect(Collectors.toList());
+
+
+
+        return false;
+    }
 }
