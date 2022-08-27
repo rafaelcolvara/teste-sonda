bba0918f418980791fc88c1f2a50bd9360a761cc
Rafael Colvara <rafael.colvara@valemobi.com.br>
2022-08-27 20:26:10 -0300

    Create DTO to relationate Probes and Planets

--- src/main/java/br/com/elo7/sonda/candidato/dto/ProbesOnPlanetDTO.java
+++ src/main/java/br/com/elo7/sonda/candidato/dto/ProbesOnPlanetDTO.java
@@ -1,2 +1,18 @@
-package br.com.elo7.sonda.candidato.dto;public class ProbesOnPlanetDTO {
+package br.com.elo7.sonda.candidato.dto;
+
+import java.util.List;
+
+public class ProbesOnPlanetDTO {
+
+    Integer idPlanet;
+
+    List<Integer> idProbes;
+
+    public Integer getIdPlanet() {
+        return idPlanet;
+    }
+
+    public List<Integer> getIdProbes() {
+        return idProbes;
+    }
 }
