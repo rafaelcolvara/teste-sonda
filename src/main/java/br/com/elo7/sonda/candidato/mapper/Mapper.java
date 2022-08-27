21478285dd1a8cb9376fcaf08ca0de0f1aeee429
Rafael Colvara <rafael.colvara@valemobi.com.br>
2022-08-27 17:22:08 -0300

    Creating toPlanet mapper

--- src/main/java/br/com/elo7/sonda/candidato/mapper/Mapper.java
+++ src/main/java/br/com/elo7/sonda/candidato/mapper/Mapper.java
@@ -1,6 +1,7 @@
 package br.com.elo7.sonda.candidato.mapper;
 
 import br.com.elo7.sonda.candidato.dto.InputDTO;
+import br.com.elo7.sonda.candidato.dto.PlanetDTO;
 import br.com.elo7.sonda.candidato.dto.ProbeDTO;
 import br.com.elo7.sonda.candidato.model.Direction;
 import br.com.elo7.sonda.candidato.model.Planet;
@@ -20,15 +21,18 @@ public class Mapper {
     }
 
     public Planet inputDTOToPlanet(InputDTO input) {
-        Planet planet = new Planet();
-        planet.setHeight(input.getHeight());
-        planet.setWidth(input.getWidth());
+        Planet planet = new Planet(input.getWidth(), input.getHeight() );
         planet.setProbeList(toListProbes(input.getProbes(),planet));
         return planet;
     }
 
     private List<Probe> toListProbes(List<ProbeDTO> probeDTOList, Planet planet) {
         return  probeDTOList.stream().map(x -> new Probe(x.getX(), x.getY(), x.getDirection(), planet)).collect(Collectors.toList());
+
+    }
+
+    public Planet toPlanet(PlanetDTO planetDTO ) {
+        return new Planet(planetDTO.getWidth(), planetDTO.getHeight());
 
     }
 
