e93061992736a5ef9e6c9ce002945ead13849011
Rafael Colvara <rafael.colvara@valemobi.com.br>
2022-08-27 20:19:35 -0300

    Add put and get methods for Planet

--- src/main/java/br/com/elo7/sonda/candidato/controller/PlanetController.java
+++ src/main/java/br/com/elo7/sonda/candidato/controller/PlanetController.java
@@ -1,12 +1,15 @@
 package br.com.elo7.sonda.candidato.controller;
 
 import br.com.elo7.sonda.candidato.dto.PlanetDTO;
+import br.com.elo7.sonda.candidato.dto.ProbesOnPlanetDTO;
 import br.com.elo7.sonda.candidato.model.Planet;
 import br.com.elo7.sonda.candidato.service.PlanetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/planet")
@@ -15,15 +18,24 @@ public class PlanetController {
     @Autowired
     PlanetService planetService;
 
-    @PostMapping
+    @PostMapping("/save")
     public ResponseEntity<Planet> registerPlanet(@RequestBody PlanetDTO planetDTO) {
         return ResponseEntity.ok(planetService.savePlanet(planetDTO));
     }
 
-    @GetMapping
-    public ResponseEntity<Planet> getPlanet(@RequestParam Integer id) {
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<Planet> getPlanet(@PathVariable Integer id) {
         return ResponseEntity.ok(planetService.getPlanetById(id));
     }
 
+    @PutMapping("/setProbes")
+    public ResponseEntity<Planet> setProbe(@RequestBody ProbesOnPlanetDTO probesOnPlanetDTO ) throws Exception {
+        return ResponseEntity.ok(planetService.setProbe(probesOnPlanetDTO));
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Planet>> getAllPlanets() {
+        return ResponseEntity.ok(planetService.getAllPlanets());
+    }
 
 }
