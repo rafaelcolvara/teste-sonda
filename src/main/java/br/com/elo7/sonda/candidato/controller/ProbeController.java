82c202bc730f142bbcbe7a28c7fd216cfdbb0b6e
Rafael Colvara <rafael.colvara@valemobi.com.br>
2022-08-27 20:20:58 -0300

    Add post and get methods for Probe

--- src/main/java/br/com/elo7/sonda/candidato/controller/ProbeController.java
+++ src/main/java/br/com/elo7/sonda/candidato/controller/ProbeController.java
@@ -1,11 +1,13 @@
 package br.com.elo7.sonda.candidato.controller;
 
 import br.com.elo7.sonda.candidato.dto.InputDTO;
+import br.com.elo7.sonda.candidato.dto.ProbeDTO;
 import br.com.elo7.sonda.candidato.model.Probe;
 import br.com.elo7.sonda.candidato.service.ProbeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,25 @@ public class ProbeController {
 	@Autowired
 	private ProbeService probeService;
 
-	@PostMapping
+	@PostMapping("/land")
     public ResponseEntity<List<Probe>> register(@RequestBody InputDTO inputDto) {
 		return ResponseEntity.ok(probeService.landProbes(inputDto));
+
     }
+
+	@PostMapping("/save")
+	public ResponseEntity<List<Probe>> saveProbes(@RequestBody List<ProbeDTO> probeDTOList) {
+		return ResponseEntity.ok(probeService.saveProbes(probeDTOList));
+	}
+
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Probe>> getAllProbes() {
+		return ResponseEntity.ok(probeService.getAllProbes());
+	}
+
+
+
+
+
 
 }
