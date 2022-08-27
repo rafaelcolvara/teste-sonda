e58377d3a24530f85b529866dd38b353af6c34a8
Rafael Colvara <rafael.colvara@valemobi.com.br>
2022-08-27 16:41:35 -0300

    Add mappert toListProbes

--- src/main/java/br/com/elo7/sonda/candidato/mapper/Mapper.java
+++ src/main/java/br/com/elo7/sonda/candidato/mapper/Mapper.java
@@ -2,22 +2,20 @@ package br.com.elo7.sonda.candidato.mapper;
 
 import br.com.elo7.sonda.candidato.dto.InputDTO;
 import br.com.elo7.sonda.candidato.dto.ProbeDTO;
+import br.com.elo7.sonda.candidato.model.Direction;
 import br.com.elo7.sonda.candidato.model.Planet;
 import br.com.elo7.sonda.candidato.model.Probe;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class Mapper {
 
     public Probe toProbe(ProbeDTO probeDto, Planet planet) {
 
-        Probe probe = new Probe();
-        probe.setPlanet(planet);
-        probe.setX(probeDto.getX());
-        probe.setY(probeDto.getY());
-        probe.setDirection(probeDto.getDirection());
+        Probe probe = new Probe(probeDto.getX(), probeDto.getY(), probeDto.getDirection(), planet);
         return probe;
     }
 
@@ -25,8 +23,13 @@ public class Mapper {
         Planet planet = new Planet();
         planet.setHeight(input.getHeight());
         planet.setWidth(input.getWidth());
+        planet.setProbeList(toListProbes(input.getProbes(),planet));
         return planet;
     }
 
+    private List<Probe> toListProbes(List<ProbeDTO> probeDTOList, Planet planet) {
+        return  probeDTOList.stream().map(x -> new Probe(x.getX(), x.getY(), x.getDirection(), planet)).collect(Collectors.toList());
+
+    }
 
 }
