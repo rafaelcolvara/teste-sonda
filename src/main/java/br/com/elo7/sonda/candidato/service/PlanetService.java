98c4fe9970766245825707cbeb97de2708952c1c
Rafael Colvara <rafael.colvara@valemobi.com.br>
2022-08-27 17:24:41 -0300

    create planetService

--- src/main/java/br/com/elo7/sonda/candidato/service/PlanetService.java
+++ src/main/java/br/com/elo7/sonda/candidato/service/PlanetService.java
@@ -1,2 +1,26 @@
-package br.com.elo7.sonda.candidato.service;public class PlanetService {
+package br.com.elo7.sonda.candidato.service;
+
+import br.com.elo7.sonda.candidato.dto.PlanetDTO;
+import br.com.elo7.sonda.candidato.mapper.Mapper;
+import br.com.elo7.sonda.candidato.model.Planet;
+import br.com.elo7.sonda.candidato.persistence.Planets;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PlanetService {
+
+    @Autowired
+    private Planets planets;
+
+    @Autowired
+    private Mapper mapper;
+
+    public Planet savePlanet(PlanetDTO planetDTO){
+       return planets.save(mapper.toPlanet(planetDTO));
+    }
+
+    public Planet getPlanetById(Integer id) {
+
+        return planets.getById(id);
+
+    }
 }
